cfc9e2fce8c9201643148ceab92a7b1ad637ea6e
Carlos Reyes <caditosdeyes2002@hotmail.com>
2023-11-20 14:05:12 -0600

    Aplicación terminada, solo falta mostrar los registros en las páginas web0, respaldo de editar.php

--- bd/editar.php
+++ bd/editar.php
@@ -1,42 +1,48 @@
 <?php
-// Variables internas = variables externas
-$nombre = $_GET['nombre'];
-$apellido = $_GET['apellido'];
-$var1 = $_GET['nuevoNombre'];
-$var2 = $_GET['nuevoApellido'];
-$var3 = $_GET['marcaCarro'];
-$var4 = $_GET['placasCarro'];
-$var5 = $_GET['colorCarro'];
-$var6 = $_GET['horaEntrada'];
-$var7 = $_GET['diaEntrada'];
-$var8 = $_GET['puertaEntrada'];
-$var9 = $_GET['moduloVisita'];
-
 // Datos de conexión
 $server = "localhost";
 $user = "id21510609_admin";
 $pass = "#Admin1234";
 $bd = "id21510609_datos";
 
+// Crear conexión
 $cone = mysqli_connect($server, $user, $pass, $bd);
+
+// Verificar la conexión
 if (!$cone) {
-    die("Error al conectar");
+    die("Error al conectar: " . mysqli_connect_error());
 }
 
-// Crear la sentencia de actualización
-$sql = "UPDATE DatosP SET Nombre='$var1', Apellido='$var2', MarcaCarro='$var3', PlacasCarro='$var4', ColorCarro='$var5', HoraEntrada='$var6', DiaEntrada='$var7', PuertaEntrada='$var8', ModuloVisita='$var9' WHERE Nombre='$nombre' AND Apellido='$apellido'";
+// Recibir datos del POST
+$id = $_POST['id'];
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
+$marcaCarro = $_POST['marcaCarro'];
+$placasCarro = $_POST['placasCarro'];
+$colorCarro = $_POST['colorCarro'];
+$horaEntrada = $_POST['horaEntrada'];
+$diaEntrada = $_POST['diaEntrada'];
+$puertaEntrada = $_POST['puertaEntrada'];
+$moduloVisita = $_POST['moduloVisita'];
 
-// Ejecutar sentencia de actualización
+// Crear la sentencia de actualización
+$sql = "UPDATE DatosP SET Nombre='$nombre', Apellido='$apellido', MarcaCarro='$marcaCarro', PlacasCarro='$placasCarro', ColorCarro='$colorCarro', HoraEntrada='$horaEntrada', DiaEntrada='$diaEntrada', PuertaEntrada='$puertaEntrada', ModuloVisita='$moduloVisita' WHERE id=$id";
+
+// Ejecutar la sentencia de actualización
 if (mysqli_query($cone, $sql)) {
     // Obtener los datos actualizados después de la edición
-    $result = mysqli_query($cone, "SELECT * FROM DatosP WHERE Nombre='$var1' AND Apellido='$var2'");
+    $result = mysqli_query($cone, "SELECT * FROM DatosP WHERE id=$id");
     $row = mysqli_fetch_assoc($result);
 
+    // Imprimir información de depuración
+    error_log('Datos actualizados: ' . json_encode($row));
     // Devolver los datos como JSON
     echo json_encode($row);
 } else {
-    // Devolver un mensaje de error en formato JSON
-    echo json_encode(array("error" => "Error al actualizar la cita."));
+    // Imprimir mensaje de error en formato JSON
+    $error_message = mysqli_error($cone); // Obtener el mensaje de error de MySQL
+    error_log('Error al actualizar la cita. Detalles: ' . $error_message);
+    echo json_encode(array("error" => "Error al actualizar la cita. Detalles: " . $error_message));
 }
 
 // Cerrar la conexión
